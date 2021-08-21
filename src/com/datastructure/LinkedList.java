3f19bbcd8d77a34794fd1b5ad7d878fa7a0a60de
sreeliptaa <sreelipta65@gmail.com>
2021-08-21 13:15:07 +0530

    Added: welcome message

---
+++ src/com/datastructure/LinkedList.java
@@ -0,0 +1,7 @@
+package com.datastructure;
+
+public class LinkedList {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Data Structure Programs using Java Generics");
+    }
+}
